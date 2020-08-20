89d76111b5dbe1fd64555b91ab03d649ac58c222
ashutoshbisoyi <ashutoshbisoyi205@gmail.com>
2020-08-20 22:50:44 +0530

    Updates

---
+++ precache-manifest.3ad6cf10b3e2ccda929fd2ef3dc36854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba52612dd0bd1ff3588c8a1f0b19014",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "7c61ab2ad1bcc739fb10",
+    "url": "/todolist/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "16ccea61ca8b7f0c67e2",
+    "url": "/todolist/static/css/main.09464c09.chunk.css"
+  },
+  {
+    "revision": "7c61ab2ad1bcc739fb10",
+    "url": "/todolist/static/js/2.e1fa4807.chunk.js"
+  },
+  {
+    "revision": "89d731e2f9540bc85ed140f44506075c",
+    "url": "/todolist/static/js/2.e1fa4807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ccea61ca8b7f0c67e2",
+    "url": "/todolist/static/js/main.d4c580ab.chunk.js"
+  },
+  {
+    "revision": "bee46aa5590999326f5b",
+    "url": "/todolist/static/js/runtime-main.3275c7e1.js"
+  }
+]);
